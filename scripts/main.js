3494977156c719b4597e7929d59cc85d050d0d85
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-18 14:38:58 +0300

    add pole & arrows

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const gblocklink = document.querySelector('.game-block');
+const arruplink = document.querySelector('.arrow-up');
+const arrdownlink = document.querySelector('.arrow-down');
+const arrleftlink = document.querySelector('.arrow-left');
+const arrrightlink = document.querySelector('.arrow-right');
