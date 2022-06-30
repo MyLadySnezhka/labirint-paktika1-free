568dff95f919d87c3d31d9dc6360766be5eb60f3
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-30 14:24:39 +0300

    add arrow block

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,82 @@
 const gblocklink = document.querySelector('.game-block');
-const arruplink = document.querySelector('.arrow-up');
-const arrdownlink = document.querySelector('.arrow-down');
-const arrleftlink = document.querySelector('.arrow-left');
-const arrrightlink = document.querySelector('.arrow-right');
+const buttonlink = document.querySelector('.arrow');
+const arrUpLink = document.querySelector('button[name="arrow-up"]');
+const arrDownLink = document.querySelector('button[name="arrow-down"]');
+const arrLeftLink = document.querySelector('button[name="arrow-left"]');
+const arrRightLink = document.querySelector('button[name="arrow-right"]');
+
+/*
+let napr, znak;
+
+const arrowjump = (buttonlink) => {
+    buttonlink.addEventListener('click', (ev) => {
+        //ТУТ НУЖНО СЧИТЫВАТЬ ТЕКУЩИЕ КООРДИНАТЫ
+    gblocklink.style.top += '10px';
+}
+    )};
+
+    arrowjump(arrUpLink);
+*/
+
+let coordY, coordX;
+let poleHeight, poleWidth;
+
+const numRowLnk = document.querySelector('input[name="numberRow"]');
+const numColLnk = document.querySelector('input[name="numberColumn"]');
+
+const startBtnLink = document.querySelector('button[name="startBtn"]');
+
+const poleLnk = document.querySelector('.pole');
+
+startBtnLink.addEventListener('click', (ev) => {
+    const row = numRowLnk.value; 
+    const col = numColLnk.value;
+    poleHeight = row*100;
+    poleWidth = col*100;
+    poleLnk.style.height = `${poleHeight}px`;
+    poleLnk.style.width = `${poleWidth}px`;
+    
+    
+    //плохо работает с нечетным количеством строк
+    gblocklink.style.top = `${row*100/2}px`;
+    gblocklink.style.left = `${col*100/2}px`;  
+    
+})
+
+arrUpLink.addEventListener('click', (ev) => {
+    coordY = parseInt(gblocklink.style.top);
+    coordY = coordY - 100;
+    if (coordY>=0)  
+        {gblocklink.style.top = `${coordY}px`;}
+    return;
+})
+
+arrDownLink.addEventListener('click', (ev) => {
+    coordY = parseInt(gblocklink.style.top);
+    coordY = coordY + 100;
+    if (coordY<=poleHeight-100)
+        {gblocklink.style.top = `${coordY}px`;}
+    return;
+})
+
+arrLeftLink.addEventListener('click', (ev) => {
+    coordX = parseInt(gblocklink.style.left);
+    coordX = coordX - 100;
+    if (coordX>=0)
+        {gblocklink.style.left = `${coordX}px`;}
+    return;
+})
+
+arrRightLink.addEventListener('click', (ev) => {
+    coordX = parseInt(gblocklink.style.left);
+    coordX = coordX + 100;
+    if (coordX<=poleWidth-100)
+        {gblocklink.style.left = `${coordX}px`;}
+    return;
+})
+
+
+
+
+
+    
